77464624b6942a34d01a9f57e7e130f3d83c8d2d
liangmiqwq <mi_yyds@126.com>
2024-05-11 21:33:03 +0800

    feat: add some buttons

--- app/_components/Introduce.tsx
+++ app/_components/Introduce.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { LuUser } from 'react-icons/lu'
+import { MdOutlineSubdirectoryArrowLeft } from 'react-icons/md'
 import { useTranslations } from 'next-intl'
 
 import { Button } from '@/components/ui/button'
@@ -8,30 +10,41 @@ export default function Introduce() {
   return (
     <div className="flex h-5/6 w-full flex-col items-center justify-center gap-8 px-20">
       <div className="hidden items-center text-[100px] font-semibold sm:flex ">
-        {/* <h1 className="text-shadow-4xl gradient-background-emerald shadow-emerald-800 ">
-          {t('computerFirstWord')}
-        </h1> */}
         {/* <h1 className="text-shadow-4xl gradient-background-first shadow-yellow-800 "> */}
+        {/*the title on the computer*/}
         <h1 className="text-shadow-3.5xl gradient-background-first dark:text-white dark:shadow-yellow-600">
-          {t('computerFirstWord')}
+          {t('firstWord')}
         </h1>
-        {/* <h1 className="text-shadow-4xl gradient-background-blue shadow-blue-800">
-          {t('computerSecondWord')}
-        </h1> */}
-        {/* <h1 className="text-shadow-4xl ml-3 gradient-background-last shadow-sky-800"> */}
         <h1 className="text-shadow-3.5xl gradient-background-last ml-3 dark:text-white dark:shadow-sky-700">
-          {t('computerSecondWord')}
+          {t('secondWord')}
         </h1>
       </div>
-      <div className=" flex flex-col items-center gap-10 sm:hidden ">
-        <h1 className="text-8xl">{t('phoneFirstLine')}</h1>
-        <h1 className="text-8xl">{t('phoneSecondLine')}</h1>
+      <div className=" flex flex-col items-center text-8xl sm:hidden ">
+        {/*the title on the phone*/}
+        <h1 className="text-shadow-3.5xl gradient-background-first dark:text-white dark:shadow-yellow-600">
+          {t('firstWord')}
+        </h1>
+        <h1 className="text-shadow-3.5xl gradient-background-last ml-3 dark:text-white dark:shadow-sky-700">
+          {t('secondWord')}
+        </h1>
       </div>
       <div className="gradient-background-oscar text-center text-2xl font-medium">
         {t('short-description')}
       </div>
-      <Button>GET STARTED</Button>Meet the new standard for modern software
-      development. Streamline issues, sprints, and product roadmaps.
+      <div className="mt-3 flex items-center justify-center gap-5">
+        <Button>
+          {t('getStarted')}
+          <MdOutlineSubdirectoryArrowLeft className="ml-1" />
+        </Button>
+        <Button variant={'outline'}>
+          {t('joinUs')}
+          <LuUser className="ml-1" />
+        </Button>
+        <Button variant={'outline'}>
+          {t('document')}
+          <LuUser className="ml-1" />
+        </Button>
+      </div>
     </div>
   )
 }
