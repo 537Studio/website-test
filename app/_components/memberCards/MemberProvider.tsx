30ef804b949cfba0f64e0ff82c8607015ad66d82
liangmiqwq <mi_yyds@126.com>
2024-05-25 10:03:51 +0800

    feat: add computing about the number of all pages

--- app/_components/memberCards/MemberProvider.tsx
+++ app/_components/memberCards/MemberProvider.tsx
@@ -10,22 +10,45 @@ export interface MemberProviderProps {
   theNumberOfMember: number
 }
 
-export default function MemberProvider({ children, page }: MemberProviderProps) {
+export default function MemberProvider({ children, page, theNumberOfMember }: MemberProviderProps) {
   // needs: the page the user using(page), the number of all page(need calc)
-
-  const divRef = useRef(null)
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const [theNumberOfMemberInOnePage, setTheNumberOfMemberInOnePage] = useState(1)
   const [offsetWidth, setOffsetWidth] = useState(0)
+  const isAddTwenty = useRef(false)
+
+  const theNumberOfPage = useMemo(() => {
+    console.log(theNumberOfMember, theNumberOfMemberInOnePage)
+    return Math.ceil(theNumberOfMember / theNumberOfMemberInOnePage)
+  }, [theNumberOfMemberInOnePage, theNumberOfMember])
 
   const marginLeft = useMemo(() => {
-    return (offsetWidth + 20) * page
+    let addNumber = 10
+    if (isAddTwenty.current) {
+      addNumber = 20
+    }
+    // return (offsetWidth + addNumber) * (theNumberOfPage - 1)
+    // }, [theNumberOfPage, offsetWidth])
+    return (offsetWidth + addNumber) * page
   }, [page, offsetWidth])
 
-
   useEffect(() => {
-    setOffsetWidth((divRef.current as unknown as HTMLDivElement).offsetWidth)
     const handleResize = throttle(() => {
       setOffsetWidth((divRef.current as unknown as HTMLDivElement).offsetWidth)
+      // set offset width to set marginLeft
+      const windowsWidth = window.innerWidth;
+      isAddTwenty.current = true
+      if (windowsWidth >= 1280) {
+        setTheNumberOfMemberInOnePage(3)
+      } else if (windowsWidth >= 1024) {
+        setTheNumberOfMemberInOnePage(2)
+      } else {
+        setTheNumberOfMemberInOnePage(1)
+        isAddTwenty.current = false
+      }
     }, 100);
+
+    handleResize() // init 
 
     window.addEventListener('resize', handleResize);
     return () => {
