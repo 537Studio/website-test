42c811005151e94567c8cdfe392a755cc9bd2c43
liangmiqwq <mi_yyds@126.com>
2024-05-27 21:19:43 +0800

    feat: add the buttons to turn pages

--- app/_components/memberCards/MemberProvider.tsx
+++ app/_components/memberCards/MemberProvider.tsx
@@ -3,18 +3,21 @@
 'use client'
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { throttle } from 'lodash';
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { Button } from '@/components/ui/button';
 
 export interface MemberProviderProps {
   children: React.ReactNode
-  page: number
   theNumberOfMember: number
 }
 
-export default function MemberProvider({ children, page, theNumberOfMember }: MemberProviderProps) {
+export default function MemberProvider({ children, theNumberOfMember }: MemberProviderProps) {
   // needs: the page the user using(page), the number of all page(need calc)
   const divRef = useRef<HTMLDivElement | null>(null);
   const [theNumberOfMemberInOnePage, setTheNumberOfMemberInOnePage] = useState(1)
   const [offsetWidth, setOffsetWidth] = useState(0)
+  const [page, setPage] = useState(0)
   const addMarginNumber = useRef(0)
 
   const theNumberOfPage = useMemo(() => {
@@ -53,22 +56,37 @@ export default function MemberProvider({ children, page, theNumberOfMember }: Me
     }, 100);
 
     handleResize() // init 
+    setPage((theNumberOfPage + 1) * 500)
 
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div
-      // className="h-full overflow-x-auto snap-x snap-mandatory flex flex-row flex-nowrap gap-2"
-      className="h-full overflow-x-visible flex flex-row flex-nowrap gap-2"
-      ref={divRef}
-      style={{ transform: `translateX(${-marginLeft + "px"})` }}
-    // style {{ transform: "translateX((widht + 20px) * theNumberOfPage)" }}
-    >
-      {children}
-    </div>
+    <div className='h-2/3 sm:h-2/5 relative'>
+      <Button size={"icon"} variant="ghost" onClick={() => { setPage(page - 1) }}
+        className='rounded-full absolute z-10 top-0 bottom-0 left-5 my-auto bg-white dark:bg-gray-950 drop-shadow-lg'>
+        <IoIosArrowBack className='!stroke-black dark:!stroke-white' />
+      </Button>
+      <Button size={"icon"} variant="ghost" onClick={() => { setPage(page + 1) }}
+        className='rounded-full absolute z-10 top-0 bottom-0 right-5 my-auto bg-white dark:bg-gray-950 drop-shadow-lg'>
+        <IoIosArrowForward className='!stroke-black dark:!stroke-white' />
+      </Button>
+
+      <div className="overflow-hidden h-full w-[calc(100%_-_80px)] shadow-lg drop-shadow-lg bg-white m-10 p-5 mr-10 rounded-lg dark:bg-gray-900">
+        <div
+          // className="h-full overflow-x-auto snap-x snap-mandatory flex flex-row flex-nowrap gap-2"
+          className="h-full overflow-x-visible flex flex-row flex-nowrap gap-2 duration-300"
+          ref={divRef}
+          style={{ transform: `translateX(${-marginLeft + "px"})` }}
+        // style {{ transform: "translateX((widht + 20px) * theNumberOfPage)" }}
+        >
+          {children}
+        </div>
+      </div>
+    </div >
   )
 }
--- app/_components/memberCards/index.tsx
+++ app/_components/memberCards/index.tsx
@@ -18,79 +18,77 @@ export default async function MemberCards() {
   }
 
   return (
-    <div className="overflow-hidden h-2/3 sm:h-2/5 w-[calc(100%_-_80px)] shadow-lg drop-shadow-lg bg-white m-10 p-5 mr-10 rounded-lg dark:bg-black">
-      <MemberProvider page={11} theNumberOfMember={showMembers.length}>
-        {showMembers.map((member, index) => {
-          return (
-            <div
-              key={member.toString() + index}
-              className="flex snap-start flex-col justify-between p-7 w-full lg:w-1/2 xl:w-1/3 h-full flex-shrink-0"
-            >
-              {/*The avatar of the member*/}
-              <div className="flex items-center gap-3">
-                <Avatar>
-                  <AvatarImage src={member.image.src} alt={member.name} />
-                  <AvatarFallback>
-                    {member.name.charAt(0).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col justify-center">
-                  <div className="text-lg font-semibold leading-5">
-                    {member.name}
-                  </div>
-                  <div className="text-sm text-slate-800 dark:text-slate-300">
-                    {member.shortDescription.name}
-                  </div>
+    <MemberProvider theNumberOfMember={showMembers.length}>
+      {showMembers.map((member, index) => {
+        return (
+          <div
+            key={member.toString() + index}
+            className="flex snap-start flex-col justify-between p-7 w-full lg:w-1/2 xl:w-1/3 h-full flex-shrink-0"
+          >
+            {/*The avatar of the member*/}
+            <div className="flex items-center gap-3">
+              <Avatar>
+                <AvatarImage src={member.image.src} alt={member.name} />
+                <AvatarFallback>
+                  {member.name.charAt(0).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col justify-center">
+                <div className="text-lg font-semibold leading-5">
+                  {member.name}
+                </div>
+                <div className="text-sm text-slate-800 dark:text-slate-300">
+                  {member.shortDescription.name}
                 </div>
               </div>
-              {/*The description of the member*/}
-              <div className="leading-7">
-                {/*member.description*/}
-                {member.description.split('\n').map((line, index) => (
-                  <p key={line.toString() + index}>{line}</p>
-                ))}
-              </div>
-              {/*The accounts of the member*/}
-              <div className="flex items-center justify-start gap-5 flex-warp">
-                {memberContact[index].map(
-                  (contactMethod: string, indexContact) => {
-                    let isAddAt
-                    if (
-                      contactMethod === 'bilibili' ||
-                      contactMethod === 'github' ||
-                      contactMethod === 'gitee'
-                    ) {
-                      isAddAt = '@'
-                    } else {
-                      isAddAt = ''
-                    }
-                    return (
-                      <>
-                        <div
-                          className="flex flex-col items-center"
-                          key={contactMethod + index + indexContact}
-                        >
-                          <div>{capitalizeWords(contactMethod)}</div>
-                          <a href={(showMembers[index][contactMethod] as membersPlatform).href} className='font-medium text-primary underline underline-offset-4'>
-                            {
-                              isAddAt + (
-                                showMembers[index][
-                                contactMethod
-                                ] as membersPlatform
-                              ).name
-                            }
-                          </a>
-                        </div>
-                        {index === 1 ? <br /> : null}
-                      </>
-                    )
-                  },
-                )}
-              </div>
             </div>
-          )
-        })}
-      </MemberProvider>
-    </div>
+            {/*The description of the member*/}
+            <div className="leading-7">
+              {/*member.description*/}
+              {member.description.split('\n').map((line, index) => (
+                <p key={line.toString() + index}>{line}</p>
+              ))}
+            </div>
+            {/*The accounts of the member*/}
+            <div className="flex items-center justify-start gap-5 flex-warp">
+              {memberContact[index].map(
+                (contactMethod: string, indexContact) => {
+                  let isAddAt
+                  if (
+                    contactMethod === 'bilibili' ||
+                    contactMethod === 'github' ||
+                    contactMethod === 'gitee'
+                  ) {
+                    isAddAt = '@'
+                  } else {
+                    isAddAt = ''
+                  }
+                  return (
+                    <>
+                      <div
+                        className="flex flex-col items-center"
+                        key={contactMethod + index + indexContact}
+                      >
+                        <div>{capitalizeWords(contactMethod)}</div>
+                        <a href={(showMembers[index][contactMethod] as membersPlatform).href} className='font-medium text-primary underline underline-offset-4'>
+                          {
+                            isAddAt + (
+                              showMembers[index][
+                              contactMethod
+                              ] as membersPlatform
+                            ).name
+                          }
+                        </a>
+                      </div>
+                      {index === 1 ? <br /> : null}
+                    </>
+                  )
+                },
+              )}
+            </div>
+          </div>
+        )
+      })}
+    </MemberProvider>
   )
 }
