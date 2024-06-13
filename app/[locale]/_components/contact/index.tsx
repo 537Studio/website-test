5dd4782ffc10790711e7e02f755b937aac846d29
liangmiqwq <mi_yyds@126.com>
2024-06-13 21:15:10 +0800

    feat: add title for the contacts

--- app/[locale]/_components/contact/index.tsx
+++ app/[locale]/_components/contact/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useTranslations } from 'next-intl'
 
 export default function Contact() {
+  const t = useTranslations()
+
   return (
     <div className="m-10">
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
-        You can find us here:
+        {t('FindUs')}
       </h3>
     </div>
   )
