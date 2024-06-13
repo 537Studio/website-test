a99256a1403d84fd06dc7358f2e66a25a3671880
liangmiqwq <mi_yyds@126.com>
2024-06-13 21:04:28 +0800

    feat: init contacts

--- app/[locale]/_components/contact/index.tsx
+++ app/[locale]/_components/contact/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export default function Contact() {
-  return <div>index</div>
+  return (
+    <div className="m-10">
+      <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+        You can find us here:
+      </h3>
+    </div>
+  )
 }
