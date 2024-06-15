9dcdf3d196e1fbf14c9ed92b061185906bf51d02
liangmiqwq <mi_yyds@126.com>
2024-06-15 12:50:05 +0800

    feat: contact cards show

--- app/[locale]/_components/contact/index.tsx
+++ app/[locale]/_components/contact/index.tsx
@@ -12,9 +12,23 @@ export default function Contact() {
       <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
         {t('FindUs')}
       </h3>
-      <div className="mt-10 grid gap-10 sm:[grid:140px_/_auto_auto] md:[grid:160px_/_auto_auto_auto] lg:[grid:180px_/_auto_auto_auto_auto] xl:[grid:200px_/_auto_auto_auto_auto_auto]">
+      <div className="mt-10 grid gap-3 [grid:150px_150px_150px_/_50%_50%] sm:gap-10 sm:[grid:240px_240px_240px_/_auto_auto] md:[grid:220px_220px_/_auto_auto_auto] lg:[grid:200px_200px_/_auto_auto_auto_auto] xl:[grid:200px_/_auto_auto_auto_auto_auto]">
         {contacts.map((contact, index) => {
-          return <div key={contact.toString() + index}>111</div>
+          const icon = React.cloneElement(contact.icon, {
+            className: '',
+            size: 40,
+          })
+          return (
+            <div
+              key={contact.toString() + index}
+              className="flex flex-col items-center justify-center gap-8 rounded-lg p-5 shadow-lg shadow-slate-400 sm:py-10"
+            >
+              {icon}
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                {contact.name}
+              </h4>
+            </div>
+          )
         })}
       </div>
     </div>
